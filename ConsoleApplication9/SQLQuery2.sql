7f714a9078d5dcb138fdb046eaeee5b0977b1f70
KFU-user <KFU-user@N-1511375168>
2016-11-05 09:00:09 +0300

    sql

---
+++ ConsoleApplication9/SQLQuery2.sql
@@ -0,0 +1,68 @@
+﻿--select onum, odate, amt from Orders
+
+--select cnum, city, rating from Customers
+
+--select * from Customers
+--	where Customers.snum = 1001
+
+--select * from Orders
+--	where Orders.snum	=	1001
+
+--select city, sname, snum, comm from Salespeople
+
+--select city, snum, cname, cnum, rating from Customers
+
+--select rating, cname from Customers
+--	where Customers.city = 'San Jose'
+
+--select sname, N' из города' as fromCity, city from Salespeople
+
+--select * from Salespeople
+--	where	Salespeople.comm > 0.12
+
+--select sname, comm from Salespeople
+--	where Salespeople.city = 'London'
+
+--select distinct city from Salespeople
+
+--select distinct snum from Orders
+
+--select distinct cnum from Orders
+
+--select * from Orders
+--	where Orders.amt > 1000
+
+--select * from Customers
+--	where Customers.rating >150
+
+--select sname, city from salespeople
+--	where salespeople.city = 'london'	and	salespeople.comm > 0.10
+
+--select cname, city from Customers
+--	where Customers.city = 'San Jose' AND Customers.rating > 150
+
+--select * from Customers
+--	where Customers.city != 'Rome' AND Customers.rating <= 150
+
+--select * from Salespeople	
+--	where Salespeople.city != 'London' AND Salespeople.comm > 0.11
+
+--select * from Salespeople
+--	where (Salespeople.city = 'London' AND Salespeople.comm <= 0.11) or (Salespeople.city != 'London' AND Salespeople.comm > 0.13)
+
+--select * from Customers
+--	where	cnum in (2001, 2003, 2007)
+
+--select * from Customers
+--	where cnum between 2002 AND 2005
+
+--select * from Customers
+--	where cname between 'C' AND 'H'
+
+--select * from Customers
+--	where cname like 'C%'
+
+--select * from Customers
+--	where cname like 'L__'
+
+
---
+++ ConsoleApplication9/SQLQuery3.sql
@@ -0,0 +1,83 @@
+﻿--select * from orders
+--	where odate in('03.10.2015','04.10.2015')
+
+--select * from orders
+--	where snum in(1001, 1003, 1007)
+
+--select sum(amt) from Orders
+--	where  odate = '03.10.2015'
+
+--select count(distinct city) from Customers
+
+--select cnum, min(amt) minAmt from Orders
+--	group by cnum
+
+--select cname from Customers
+--	where cname like ('G%')
+--		order by cname asc
+
+--select city, max(rating) from Customers
+--	group by city
+
+--select count(distinct cnum), odate from orders
+--	group by odate
+
+--select 'For the city', city, 'the highest rating is:', max(rating) from Customers
+--	group by city
+
+--select cname, cnum, rating from Customers
+--	order by rating desc
+
+--select sum(amt) as sumAmt, odate from Orders
+--	group by odate
+--		order by sumAmt desc
+
+--select onum, orders.snum, (comm*amt) as commAmt from orders
+--	join Salespeople
+--		on Salespeople.snum = Orders.snum
+
+--select onum, cname from Orders, Customers
+--	where Orders.cnum = Customers.cnum
+
+--select onum, cname, odate, amt from Orders
+--	join Customers 
+--		on Orders.cnum = Customers.cnum
+
+--select onum, sname, cname from Orders	
+--	join Salespeople 
+--		on Salespeople.snum = Orders.snum
+--	join Customers
+--		on Customers.cnum = Orders.cnum
+
+--select cname, sname, comm from Customers
+--	join Salespeople
+--		on Salespeople.snum = Customers.snum
+--			where comm > 0.12
+	
+--select Salespeople.snum, sum(comm*amt) as sumComm from Orders
+--	join Salespeople 
+--		on Salespeople.snum = Orders.snum
+--	join Customers
+--		on Customers.cnum = Orders.cnum
+--			where rating > 100
+--	group by Salespeople.snum
+
+--select s1.city, s1.sname, s2.sname from Salespeople as s1
+--	join Salespeople as s2
+--		on s1.city = s2.city
+--			where s1.snum < s2.snum
+
+--select cname, o1.onum, o2.onum from Orders as o1
+--	join Orders as o2 
+--		on o1.cnum = o2.cnum
+--	join Customers
+--		on Customers.cnum = o1.cnum
+--	where o1.onum < o2.onum
+
+--select c.cname, c.city, c.rating from Customers as c
+--	join Customers as cH
+--		on cH.rating = c.rating
+--			where cH.cnum = 2001 and c.cnum != cH.cnum
+--	--where rating = (select rating from Customers where cnum = 2001)
+
+
